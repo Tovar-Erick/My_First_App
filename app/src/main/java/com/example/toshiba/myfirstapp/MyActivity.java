55125668802802b359a6a5b647384c38e35ca3fa
Tovar-Erick <sephiroth_eri@hotmail.com>
2015-11-12 00:24:18 -0600

    Completed Building Your First App Class TODO: Supporting Different Devices Class

--- app/src/main/java/com/example/toshiba/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/toshiba/myfirstapp/MyActivity.java
@@ -1,13 +1,16 @@
 package com.example.toshiba.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MyActivity extends ActionBarActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.toshiba.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,5 +37,14 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Called when user clicks the Send button
+    public void sendMessage(View p_view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
